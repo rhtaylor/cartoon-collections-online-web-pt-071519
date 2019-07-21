710016614e00aa7e7f2534c6e501e5210a7e3af0
Randall Taylor <randyisbusy@gmail.com>
2019-07-21 03:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ def summon_captain_planet(arg)
 end
 
 def long_planeteer_calls(arg)
-  arg.any?{ |x| x.length >= 4 }
+  arg.any?{ |x| x.length > 4 }
 end
 
 def find_the_cheese# code an argument here
