a320ccfc6ce6c1beeb42cfe2d2eef6337d0299e1
Randall Taylor <randyisbusy@gmail.com>
2019-07-21 03:31:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(args)
+    args.each_with_index{ |x,i| puts "#{i +1} #{x}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(arg)
+
+   x = arg.each{ |x| x.first.upcase + "!" }
+   return x
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arg)
+
 end
 
 def find_the_cheese# code an argument here
